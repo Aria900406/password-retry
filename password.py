23cff0d370cb88940366c4b8318b270f6afea719
Aria Wang <aria900406@gmail.com>
2023-07-25 19:03:35 +0800

    version2

--- password.py
+++ password.py
@@ -1,14 +1,14 @@
 password = 'a123456'
 x = 3 #剩餘機會
 while x > 0:
+    x = x - 1
     pwd = input('請輸入密碼: ')
     if pwd == password:
         print('登入成功!')
         break #跳出迴圈
     else: 
-        x = x - 1
-        print('密碼錯誤! 還有', x,'次機會')
-
-        
-
- 
+        print('密碼錯誤!')
+        if x > 0:
+            print('還有', x,'次機會')
+        else:
+            print('沒機會了，帳號已鎖')
